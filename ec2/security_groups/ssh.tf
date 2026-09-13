2df74731ef3c3679cc5434d08a5b85a365c916f1
Levantine <github@levantine.io>
2026-09-12 22:16:47 -0700

    Update SSH security group to current home IP
    Xfinity's network changes assigned a new public IP (24.4.63.140),
    silently locking out SSH to the bastion while HTTPS stayed open to
    everyone -- ansible's levantine-ssl-renew.service failed with
    "Connection timed out" reaching bastion.prod.levantine.io on port 22
    because the security group only allowed the old IP (67.170.192.40).

--- ec2/security_groups/ssh.tf
+++ ec2/security_groups/ssh.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "sg_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["67.170.192.40/32"]
+    cidr_blocks = ["24.4.63.140/32"]
     description = "House internet"
   }
 
